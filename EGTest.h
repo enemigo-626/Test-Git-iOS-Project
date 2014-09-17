501934f918ee9c983f2fb11c7719a506e52e7cc0
Eric Garcia Ribera <EricGarciaRibera@MacBook-Pro-de-Eric.local>
2014-09-17 17:40:32 +0200

    added a test class

---
+++ EGTest.h
@@ -0,0 +1,13 @@
+//
+//  EGTest.h
+//  Git test
+//
+//  Created by Eric Garcia Ribera on 17/9/14.
+//  Copyright (c) 2014 Garcia's Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EGTest : NSObject
+
+@end
---
+++ EGTest.m
@@ -0,0 +1,13 @@
+//
+//  EGTest.m
+//  Git test
+//
+//  Created by Eric Garcia Ribera on 17/9/14.
+//  Copyright (c) 2014 Garcia's Company. All rights reserved.
+//
+
+#import "EGTest.h"
+
+@implementation EGTest
+
+@end
